a84700244b25efdd29409bc0d564e19edaffab38
buiducviettt <ducviet8a4@gmail.com>
2025-01-19 11:05:55 +0700

    react flip

--- src/components/ProductList/index.jsx
+++ src/components/ProductList/index.jsx
@@ -1,17 +1,31 @@
 import ProductItem from './ProductItem';
 import Images from '../../assets/image/Images';
+import ReactCardFlip from 'react-card-flip';
+import { useState } from 'react';
 const ProductList = () => {
+  const [isFlipped, setIsFlipped] = useState(false);
+  const handleClick = () => {
+    setIsFlipped(!isFlipped);
+  };
   return (
     <div className="product_list">
       <div className="row">
         <div className="col col-4">
           <div className="product_item item">
-            <ProductItem
-              image={Images.food1}
-              name="Spicy seasoned seafood noodles"
-              price="$ 2.29"
-              number="20"
-            />
+            <ReactCardFlip isFlipped={isFlipped} flipDirection="horizontal">
+              <div className="card-front" onClick={handleClick}>
+                <ProductItem
+                  image={Images.food1}
+                  name="Spicy seasoned seafood noodles"
+                  price="$ 2.29"
+                  number="20"
+                />
+              </div>
+              <div className="card-back" onClick={handleClick}>
+                <h3>Info</h3>
+                <p>This is some information on the back of the card!</p>
+              </div>
+            </ReactCardFlip>
           </div>
         </div>
         <div className="col col-4">
